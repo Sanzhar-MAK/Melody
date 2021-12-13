c12d7ca2d44a5db740eff8324809b902b17c7c9b
sanzhar <62831632+Sanzhar-MAK@users.noreply.github.com>
2021-12-13 20:41:46 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,11 +5,52 @@ $(document).ready(function () {
     const counterUp = $(".counter-up");
     const counterDown = $(".counter-down");
 
+    const modal = $(".modal");
+    const modalCloseButton = $(".modal-close-button");
+    const buttonPrimary = $(".button-primary");
+    const flatPath = $(".modal-image path");
+    const flatLink = $(".flat-item a");
+
+    const modalDialog = $(".modal-dialog");
+
+    const toggleModal = () => {
+        modal.toggleClass("is-open");
+    }
+
     floorPath.on('mouseover', (e) => {
         currentFloor = $(e.target).attr("data-floor");
         floorPath.removeClass("current-floor");
         $(".counter").text(currentFloor);
     });
+
+
+    flatPath.on('mouseover', e => {
+        currentFlat = $(e.target).data("flat");
+        if ($(".flat-item a:contains('" + currentFlat + "')")) {
+            currentFlatLink = $(".flat-item a:contains('" + currentFlat + "')")
+            flatLink.removeClass("flat-link-active");
+            currentFlatLink.toggleClass("flat-link-active");
+        }
+    })
+
+    flatPath.on('mouseout', () => {
+        flatLink.removeClass("flat-link-active")
+    });
+
+    flatLink.on('mouseover', e => {
+        currentFlatPosition = $(e.target).data("flat-link");
+        flatPath.removeClass("flat-active");
+        $(`[data-flat=${currentFlatPosition}]`).toggleClass("flat-active");
+    })
+
+    flatLink.on('mouseout', () => {
+        flatPath.removeClass("flat-active");
+    });
+
+
+    floorPath.on('click', toggleModal);
+    modalCloseButton.on('click', toggleModal);
+    buttonPrimary.on('click', toggleModal);
 
     counterUp.on('click', () => {
         if (currentFloor < floorPath.last().data("floor")) {
@@ -36,8 +77,5 @@ $(document).ready(function () {
             $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
         }
     })
-
-
-
 
 });
